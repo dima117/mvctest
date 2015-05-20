8d56283c239a528df969af2052a29f645230dbe5
relum <relum@yandex.ru>
2015-05-20 21:56:36 +0300

    - delete: Удалил конфликтный хелпер;

--- Mvc.Study.Beginner/Mvc.Study.WebUI/Controllers/PageMenuController.cs
+++ Mvc.Study.Beginner/Mvc.Study.WebUI/Controllers/PageMenuController.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 using System.Linq;
 using System.Web.Mvc;
-using Mvc.Study.Common;
 using Mvc.Study.Domain;
 
 namespace Mvc.Study.Beginner.Controllers
 {
     public class PageMenuController : Controller
     {
+        private Guid? _pageId
+        {
+            get
+            {
+                Guid pageId;
+                return Guid.TryParse((string) RouteData.Values["pageId"], out pageId) && pageId != Guid.Empty
+                           ? pageId
+                           : (Guid?) null;
+            }
+        }
+
         [HttpGet]
         public ActionResult Index()
         {
-            // Параметры маршрута
-            var pageId = (string) RouteData.Values["pageId"];
-
             // Модель
             var model = new PageMenuViewModel
                 {
                     Items = getItems(),
-                    SelectedItemId = ObjectHelper.ParseObject<Guid?>(pageId)
+                    SelectedItemId = _pageId
                 };
 
             return View(model);
