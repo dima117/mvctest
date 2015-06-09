c959f181a0cd77d7de3b2c8cddb704502ad05ed9
Dmitry Andriyanov <dandriyanov@ufs-online.ru>
2015-06-09 16:00:53 +0300

    - реорганизация скриптов и стилей;

--- Mvc.Study.Beginner/Mvc.Study.WebUI/App_Start/BundleConfig.cs
+++ Mvc.Study.Beginner/Mvc.Study.WebUI/App_Start/BundleConfig.cs
@@ -8,17 +8,15 @@ namespace Mvc.Study.Beginner
         {
             // Scripts
             bundles.Add(
-                new ScriptBundle("~/bundles/jquery").Include(
-                    "~/Scripts/jquery-2.1.4.min.js"));
-            bundles.Add(
-                new ScriptBundle("~/bundles/modernizr").Include(
-                    "~/Scripts/modernizr-2.8.3.js"));
-
+				new ScriptBundle("~/Content/js").Include(
+					"~/Content/js/jquery-2.1.4.min.js",
+					"~/Content/js/bootstrap.min.js"));
+         
             // CSS
             bundles.Add(
                 new StyleBundle("~/Content/css").Include(
-                    "~/Content/site.css",
-                    "~/Content/bootstrap.min.css"));
+					"~/Content/css/bootstrap.min.css",
+                    "~/Content/css/site.css"));
         }
     }
 }
